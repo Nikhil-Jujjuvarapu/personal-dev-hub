85ea4bf3ec9d9d64433ef5ceccffdf349fbf08b1
Nikhil Jujjuvarapu <jnikhil2000@gmail.com>
2025-11-03 16:17:04 +0530

    Root module change

--- DevOps/AWS/root/main.tf
+++ DevOps/AWS/root/main.tf
@@ -1,25 +1,69 @@
 // Root module to wire Lambda and S3 modules together
 // Instantiates the Lambda module and passes its role ARN into the S3 module
 
-module "lambda" {
-  source           = "../modules/Lambda"
+module "iam" {
+  source = "../modules/IAM"
 
-  account_id       = var.account_id
-  iam_user_name    = var.iam_user_name
-  region           = var.region
-  bucket_name      = var.bucket_name
-  s3_lambda_fn_name = var.s3_lambda_fn_name
+  account_id    = var.account_id
+  iam_user_name = var.iam_user_name
+  region        = var.region
 }
 
 module "s3" {
-  source           = "../modules/S3"
+  source = "../modules/S3"
+
+  account_id       = var.account_id
+  iam_user_name    = var.iam_user_name
+  region            = var.region
+  bucket_name       = var.bucket_name
+  s3_lambda_fn_name = var.s3_lambda_fn_name
+  lambda_role_arn   = module.lambda.lambda_exec_role_arn
+}
+
+module "lambda" {
+  source = "../modules/Lambda"
 
   account_id       = var.account_id
   iam_user_name    = var.iam_user_name
   region           = var.region
   bucket_name      = var.bucket_name
   s3_lambda_fn_name = var.s3_lambda_fn_name
+  get_metadata_fn  = var.get_metadata_fn
+  put_metadata_fn  = var.put_metadata_fn
+  table_name       = var.table_name
+}
 
-  # pass the role ARN output from the Lambda module
+module "dynamodb" {
+  source = "../modules/dynamoDb"
+
+  account_id    = var.account_id
+  iam_user_name = var.iam_user_name
+  region        = var.region
+  bucket_name   = var.bucket_name
+  s3_lambda_fn_name = var.s3_lambda_fn_name
+  table_name    = var.table_name
+}
+
+module "cognito" {
+  source = "../modules/Cognito"
+
+  account_id     = var.account_id
+  iam_user_name  = var.iam_user_name
+  region         = var.region
+  bucket_name    = var.bucket_name
+  user_pool_name = var.user_pool_name
+  app_client_name = var.app_client_name
+}
+
+module "apigateway" {
+  source = "../modules/apigateway"
+
+  account_id       = var.account_id
+  iam_user_name    = var.iam_user_name
+  region           = var.region
+  bucket_name      = var.bucket_name
+  s3_lambda_fn_name = var.s3_lambda_fn_name
+  api_name         = var.api_name
+  stage_name       = var.stage_name
   lambda_role_arn  = module.lambda.lambda_exec_role_arn
 }
--- DevOps/AWS/root/variables.tf
+++ DevOps/AWS/root/variables.tf
@@ -22,3 +22,45 @@ variable "s3_lambda_fn_name" {
   description = "Lambda function name used for S3 notifications"
   type        = string
 }
+
+variable "get_metadata_fn" {
+  description = "Lambda function name for GET metadata"
+  type        = string
+  default     = "get_metadata_fn"
+}
+
+variable "put_metadata_fn" {
+  description = "Lambda function name for PUT metadata"
+  type        = string
+  default     = "put_metadata_fn"
+}
+
+variable "table_name" {
+  description = "DynamoDB table name"
+  type        = string
+  default     = "ImageMetadata"
+}
+
+variable "api_name" {
+  description = "API Gateway name"
+  type        = string
+  default     = "DEV-API"
+}
+
+variable "stage_name" {
+  description = "API Gateway stage name"
+  type        = string
+  default     = "v1"
+}
+
+variable "user_pool_name" {
+  description = "Cognito user pool name"
+  type        = string
+  default     = "dev_user_pool"
+}
+
+variable "app_client_name" {
+  description = "Cognito app client name"
+  type        = string
+  default     = "dev_app_client"
+}
